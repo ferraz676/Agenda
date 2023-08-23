2816397a9556973d763cc3e1295c8da59efbf3df
ferraz676 <mat_ferraz12@hotmail.com>
2023-08-23 17:32:34 -0300

    ok

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from'cors';
+
+
+import agendaController from './controller/agendaController.js';
+
+let servidor = express();
+servidor.use(cors());
+servidor.use(express.json());
+
+servidor.use(agendaController);
+
+servidor.listen(process.env.PORT, () => console.log('API subiu!'));
